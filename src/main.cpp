c68c48a4429b89d9f6608f65f1bf1a4166506b70
Michael Eskelin <michael.eskelin@gmail.com>
2025-09-11 23:04:48 -0400

    added different stuff. score, game loss, ball return from the top of screen, etc

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 // Define variables for later
 const int screenWidth = 1280;
 const int screenHeight = 600;
+const char windowTitle[9] = "Breakout";
 const int targetFPS = 60;
 const int blockWidth = 80;
 const int blockHeight = 20;
@@ -14,10 +15,13 @@ const int rows = 5;
 const int cols = screenWidth / blockWidth;
 const int totalBlocks = rows * cols;
 const int ballSpeed = 30;
+
 int frames;
 int score;
 bool upwardBallMovement = true;
+// bool rightBallMovement = true;
 bool ballBoxCollision = false;
+bool paddleCollision = false;
 
 /* 
  * Block struct
@@ -25,6 +29,7 @@ bool ballBoxCollision = false;
  * y: y pos
  * texture: texture for block
  * shown: drawn or not (used for later when ball destroys the blocks)
+ * collisionRect: Rectangle used for collision purposes
 */
 struct Block {
 	int x;
@@ -50,7 +55,8 @@ std::vector<Block> populateBlockTable(std::vector<Texture2D> blockTextureTable) 
 		tb.y = row * blockHeight;
 		tb.texture = blockTextureTable[row];
 		tb.shown = true;
-		tb.collisionRect = Rectangle({static_cast< float >(tb.x), static_cast< float >(tb.y), static_cast< float >(tb.texture.width), static_cast< float >(tb.texture.height)});
+		tb.collisionRect = Rectangle({static_cast< float >(tb.x), static_cast< float >(tb.y), 
+		   							  static_cast< float >(tb.texture.width), static_cast< float >(tb.texture.height)});
 		bt.push_back(tb);
 	}
 	return bt;
@@ -59,8 +65,9 @@ std::vector<Block> populateBlockTable(std::vector<Texture2D> blockTextureTable) 
 int main(void) {
 	// Seed the generator for random numbers (used for initial ball trajectory)
 	srand(time(NULL));
-	InitWindow(screenWidth, screenHeight, "Breakout");	
+	InitWindow(screenWidth, screenHeight, windowTitle);	
 	SetTargetFPS(targetFPS);
+
 	// Load block images and textures
 	Image blueBlockImage = LoadImage("assets/blueblock.png");
 	Texture2D blueBlockTexture = LoadTextureFromImage(blueBlockImage);
@@ -91,20 +98,28 @@ int main(void) {
 	Texture2D ballTexture = LoadTextureFromImage(ballImage);
 	Block ball;
 	ball.texture = ballTexture;
-	ball.x = screenWidth / 2 - ball.texture.width / 2;
-	ball.y = playerBlock.y - ball.texture.height;
-	ball.collisionRect = Rectangle({static_cast<float>(ball.x), static_cast<float>(ball.y), static_cast<float>(ball.texture.width), static_cast<float>(ball.texture.height)});
+	ball.x = rand() % screenWidth - ball.texture.width;
+	ball.y = (playerBlock.y - ball.texture.height) - 1;
+	ball.collisionRect = Rectangle({static_cast<float>(ball.x), static_cast<float>(ball.y), 
+									static_cast<float>(ball.texture.width), static_cast<float>(ball.texture.height)});
 	ball.shown = true;
 	
+	std::cout << playerBlock.x << ", " << playerBlock.y << std::endl;
+	std::cout << playerBlock.collisionRect.x << ", " << playerBlock.collisionRect.y << std::endl;
+
 	// Main game loop
     while (!WindowShouldClose())
     {
-		//Update ball's position every second
+		//Update ball's position every second, i think?
 		int delta = GetFrameTime();
 		frames++;
 		if (frames % (targetFPS / 4) == 0) {
 			if (upwardBallMovement) {
 				ball.y -= ballSpeed;
+				ball.collisionRect.y = ball.y;
+			} else {
+				ball.y += ballSpeed;
+				ball.collisionRect.y = ball.y;
 			}
 			frames = 0;
 		}
@@ -113,29 +128,52 @@ int main(void) {
 		if (IsKeyDown(KEY_RIGHT)) {
 			if (playerBlock.x < screenWidth - playerBlock.texture.width) {
 				playerBlock.x += 5;
+				playerBlock.collisionRect.x = playerBlock.x;
 			}
 		}
 
 		if (IsKeyDown(KEY_LEFT)) {
 			if (playerBlock.x > 0) {
 				playerBlock.x -= 5;
+				playerBlock.collisionRect.x = playerBlock.x;
 			}
 		}
+
+		// There is considerable lag when the ball hits the paddle, i think? Managable for now.
+		if (CheckCollisionRecs(ball.collisionRect, playerBlock.collisionRect)) {
+			ball.y = (playerBlock.y - ball.texture.height) - 1;
+			upwardBallMovement = true;
+		}
+
 		// Draw to screen
         BeginDrawing();
             ClearBackground(BLACK);
 			
 			for (int i = 0; i < blockTable.size(); i++) {
+				// Draw the blocks that haven't been destroyed
 				if (blockTable[i].shown) {
 					DrawTexture(blockTable[i].texture, blockTable[i].x, blockTable[i].y, RAYWHITE);
-				if (CheckCollisionRecs(ball.collisionRect, blockTable[i].collisionRect)) {
-					std::cout << "collision" << std::endl;
+				if (CheckCollisionRecs(ball.collisionRect, blockTable[i].collisionRect) && blockTable[i].shown) {
+					blockTable[i].shown = false;
+					upwardBallMovement = false;
+					score++;
 				}
 			}
 		}
+
+		if (ball.y >= screenHeight) {
+			std::cout << "Game over" << std::endl;
+			CloseWindow();
+		}
+
+		if (ball.y <= ball.texture.height) {
+			upwardBallMovement = false;
+		}
+
 		DrawTexture(playerBlock.texture, playerBlock.x, playerBlock.y, RAYWHITE);
 		DrawTexture(ball.texture, ball.x, ball.y, RAYWHITE);
-        EndDrawing();
+        DrawText("Score: ", screenWidth - 200, screenHeight - 40, 36, RED);
+		EndDrawing();
     }
     CloseWindow();
     return 0;
